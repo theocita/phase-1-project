f70bffec9872e18210e65629efbd50c8c93147d8
theocita <theocitaallen@gmail.com>
2022-11-06 17:47:19 -0500

    Added my first eventListener, DOMContentLoaded

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const url = 'https://api.tvmaze.com'
+// const url = 'https://api.tvmaze.com/shows'
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadAnimes()
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     loadShows()
 
-function loadAnimes() {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-}
+//     listform.addEventListener("submit", listform)
+// })
+
+// function loadShows() {
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data => data)
+// }
+
+// function listform(e) {
+//     e.preventDefault()
+
+//     const showList = document.getElementById("show-name")
+//     const li = document.createElement("li")
+//     li.textContent = showList
+
+// }
+
+const form = document.querySelector("#create-show-list")
+
+document.addEventListener("DOMContentLoaded", () => {
+    showList.addEventListener("submit", showList)
+});
+
