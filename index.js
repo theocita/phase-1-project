45708053880d452d3ab77de1f218849519df9933
theocita <theocitaallen@gmail.com>
2022-11-05 20:40:13 -0400

    Previous API was dicontinued. I modify my idea just a bit.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const url = 'https://jikan.moe/'
+const url = 'https://api.tvmaze.com'
 
 document.addEventListener('DOMContentLoaded', () => {
     loadAnimes()
 })
 
+function loadAnimes() {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
