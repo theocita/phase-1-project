ec8c297d76d495adcd89b8f9ca584e820c20f702
theocita <theocitaallen@gmail.com>
2022-11-09 19:33:09 -0500

    Api is now console logging data in console.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const url = 'https://api.tvmaze.com/shows'
+const url = `https://api.tvmaze.com/shows`
 
 // document.addEventListener('DOMContentLoaded', () => {
 //     loadShows()
@@ -21,16 +21,20 @@
 
 // }
 
-const showList = document.querySelector("#create-show-list")
+// const showList = document.querySelector("#create-show-list")
 
 document.addEventListener("DOMContentLoaded", () => {
-    showList.addEventListener("submit", submitShow)
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log("data", data))
+    // showList.addEventListener("submit", submitShow)
 });
 
-function submitShow(e) {
-   e.preventDefault()
+// function submitShow(e) {
+//    e.preventDefault()
 
-    const newShow = document.getElementById("new-show-name").value
-    const li = document.createElement("li")
-    li.textContent = newShow
-}
+//     // const newShow = document.getElementById("show-name").value
+//     // const li = document.createElement("li")
+//     // li.textContent = newShow
+// // }
