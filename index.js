8a7f004823df8b1a4a13c3639fc00de50097d0a9
theocita <theocitaallen@gmail.com>
2022-11-30 20:28:22 -0500

    I was able to pull the data and it appears in my form.

--- index.js
+++ index.js
@@ -3,57 +3,19 @@ const init = () => {
 
 const inputForm = document.querySelector('form')
 
-inputForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const input = document.querySelector("#new-show-name")
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const input = document.querySelector("#new-show-name")
   
-    fetch(`https://api.tvmaze.com/shows/${input.value}`)
-    .then(res => res.json())
-    .then(shows => {
-
-        const name = document.querySelector('show-name')
-        //name.innerText = data.name
-    
-
-        // const showList = document.getElementById("show-name")
-        // const li = document.createElement("li")
-        // li.textContent = showList      
-
-        console.log("names", shows)
-        shows.innerHTML = name
-     })
+        fetch(`https://api.tvmaze.com/shows/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const name = document.querySelector("#show-name")     
+            name.innerText = data.name
+        })
 
     })
 }
 
-
 document.addEventListener("DOMContentLoaded", init) 
-// function submitShow(e) {
-//    e.preventDefault()
-
-//     // const newShow = document.getElementById("show-name").value
-//     // const li = document.createElement("li")
-//     // li.textContent = newShow
-// // }
-// document.addEventListener('DOMContentLoaded', () => {
-//     loadShows()
-
-//     listform.addEventListener("submit", listform)
-// })
-
-// function loadShows() {
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(data => data)
-// }
-
-// function listform(e) {
-//     e.preventDefault()
-
-//     
-
-// }
-
-// const showList = document.querySelector("#create-show-list")
-   // showList.addEventListener("submit", submitShow)
  
