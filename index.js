b54f3b51bb087ff68513fc7a84f7d2bc290a412b
theocita <theocitaallen@gmail.com>
2022-11-30 21:04:39 -0500

    I moved the delete button and event listener under the fetch, it brings up the name on the first click and on the second the button appears.

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ const inputForm = document.querySelector('form')
         e.preventDefault()
         const input = document.querySelector("#new-show-name")
         const li = document.createElement("li")
-        const deleteButton = document.createElement("button")
-        deleteButton.textContent = "X"
-
-        deleteButton.addEventListener("click", () => li.remove)
-        li.append(deleteButton)
-        showname.appendChild(li)
-        inputForm.reset()
+       
   
         fetch(`https://api.tvmaze.com/shows/${input.value}`)
         .then(res => res.json())
         .then(data => {
             const name = document.querySelector("#showname")     
             name.innerText = data.name
-        })
+        }) 
+        
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = "X"
+
+         deleteButton.addEventListener("click", () => li.remove)
+        li.append(deleteButton)
+        showname.appendChild(li)
+        inputForm.reset()
 
     })
 }
