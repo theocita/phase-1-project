e1485a2de7b6fc166f05d35a074b5271a929520f
theocita <theocitaallen@gmail.com>
2022-11-30 21:01:31 -0500

    My api has failed again.

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const inputForm = document.querySelector('form')
         deleteButton.textContent = "X"
 
         deleteButton.addEventListener("click", () => li.remove)
-
+        li.append(deleteButton)
+        showname.appendChild(li)
+        inputForm.reset()
+  
         fetch(`https://api.tvmaze.com/shows/${input.value}`)
         .then(res => res.json())
         .then(data => {
