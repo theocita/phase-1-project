72118ad10110e505b22da205b4dd13a41a6095cb
theocita <theocitaallen@gmail.com>
2022-12-03 19:46:46 -0500

    I change the way i layed out the delete button but now the name of the show isnt being listed. However the button is working

--- index.js
+++ index.js
@@ -12,24 +12,29 @@ const inputForm = document.querySelector('#create-show-list')
         fetch(`https://api.tvmaze.com/shows/${input.value}`)
         .then(res => res.json())
         .then(data => {
-            const name = document.querySelector("#showname")     
-            name.innerText = data.name
-
             const li = document.createElement("li")
+            const name = document.querySelector("#showname")     
+            li.innerText = data.name
+
             const deleteButton = document.createElement("button")
+
+            deleteButton.addEventListener("click", handleDelete)
             deleteButton.textContent = "X"
-    
-            deleteButton.addEventListener("click", () => li.remove)
-            li.append(deleteButton)
-            showname.appendChild(li)
-            inputForm.reset()
-
+            li.textContent = `${name}`
+            li.appendChild(deleteButton)
+            document.querySelector("#list").appendChild(li)
+            
+           
+            // li.append(deleteButton) 
+            // showname.appendChild(li)
+            // inputForm.reset()
         }) 
-        
-       
-
+         
     })
 }
 
 document.addEventListener("DOMContentLoaded", init) 
  
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
